72b775262486afe88e8d2c246cd9495bcc944c2a
Fractal-Tess <vgfractal@gmail.com>
2025-04-13 14:10:05 +0300

    reduce measure time

--- apps/esp/src/config.h
+++ apps/esp/src/config.h
@@ -2,30 +2,31 @@
 #define CONFIG_H
 
 // Sensor Settings
-#define DHT_PIN D1          // GPIO pin connected to DHT sensor
-#define DHT_TYPE DHT11      // DHT sensor type (DHT11 or DHT22)
+#define DHT_PIN D1     // GPIO pin connected to DHT sensor
+#define DHT_TYPE DHT11 // DHT sensor type (DHT11 or DHT22)
 
 // Sleep Configuration
-#define SLEEP_TIME_SECONDS 1800  // 15 minutes = 900 seconds
+#define SLEEP_TIME_SECONDS 300 // 1lminutes = 60 seconds
 #define uS_TO_S_FACTOR 1000000ULL
 
 // Measurement Intervals
-#define MEASUREMENT_INTERVAL 300000  // 5 minutes in milliseconds
+#define MEASUREMENT_INTERVAL 300000 // 5 minutes in milliseconds
 
 // Debug Options
-#define DEBUG_SERIAL true           // Enable serial debug output
-#define DEBUG_WIFI true            // Enable WiFi debug messages
-#define DEBUG_INFLUXDB true        // Enable InfluxDB debug messages
-#define DEBUG_SENSOR true          // Enable sensor debug messages
+#define DEBUG_SERIAL true   // Enable serial debug output
+#define DEBUG_WIFI true     // Enable WiFi debug messages
+#define DEBUG_INFLUXDB true // Enable InfluxDB debug messages
+#define DEBUG_SENSOR true   // Enable sensor debug messages
 
 // Network Settings
-#define WIFI_CONNECT_TIMEOUT 1000   // 1 second between connection attempts
-#define WIFI_RECONNECT_INTERVAL 30000  // 30 seconds between WiFi reconnection attempts
-#define RETRY_INTERVAL 5000          // 5 seconds between retries
-#define MAX_RETRIES 30               // Maximum number of retries for WiFi connection
+#define WIFI_CONNECT_TIMEOUT 1000 // 1 second between connection attempts
+#define WIFI_RECONNECT_INTERVAL                                                \
+  30000                     // 30 seconds between WiFi reconnection attempts
+#define RETRY_INTERVAL 5000 // 5 seconds between retries
+#define MAX_RETRIES 30      // Maximum number of retries for WiFi connection
 
 // InfluxDB Settings
-#define INFLUXDB_TIMEOUT 5000      // 5 seconds timeout for InfluxDB operations
-#define INFLUXDB_BATCH_SIZE 1      // Number of points to batch before sending
+#define INFLUXDB_TIMEOUT 5000 // 5 seconds timeout for InfluxDB operations
+#define INFLUXDB_BATCH_SIZE 1 // Number of points to batch before sending
 
-#endif // CONFIG_H 
+#endif // CONFIG_H
